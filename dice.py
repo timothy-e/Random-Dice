e4c7bc9b924ce15a20546f18d67b6aa4c9a5c204
timothy-e <timothy.elgersma@gmail.com>
2018-02-06 00:46:38 -0500

    Add files via upload

--- dice.py
+++ dice.py
@@ -1,10 +1,6 @@
 import math
 import random
 
-sides = int(input("How many sides are on the dice? "))
-num = int(input("How many dice? "))
-trials = int(input("How many trials? "))
-
 def roll_die(s):
     return random.randrange(s) + 1
 
@@ -14,47 +10,89 @@ def get_length(n):
     else:
         return int(math.log10(n)) + 1
 
-def max_from_list(L):
+# max_space(L) returns the largest number of spaces taken up by an item in L
+def max_space(L):
     m = 0
     for i in L:
         m = max(m, get_length(i))
     return m
 
-# init arrays
-rolls = []
-for i in range(0, sides):
-    rolls.append(0)
-sums = []
-for i in range(num, num * sides + 1):
-    sums.append(0)
+def max_list(L):
+    m = L[0]
+    for i in L:
+        m = max(m, i)
+    return m
 
-# run trials
-for i in range(0, trials):
-    rolls[roll_die(sides) - 1] += 1
-    s = 0
-    for i in range(num):
-        s += roll_die(sides)
-    sums[s - 1 - num] += 1
+while (True):
+    sides = int(input("How many sides are on the dice? "))
+    num = int(input("How many dice? "))
+    trials = int(input("How many trials? "))
 
 
-# print rolls
-max_rolls = max_from_list(rolls)
-s = ""
-for i in range(0, sides):
-    s = s + " | " + str(i + 1) + (" " * max_rolls)
-print("\nRolls " + s)
-s = ""
-for i in range(0, sides):
-    s = s + " | " + str(rolls[i]) + (" " * (max_rolls - get_length(rolls[i]) + get_length(i)))
-print("      " + s)
+    # init arrays
+    rolls = []
+    for i in range(0, sides):
+        rolls.append(0)
+    sums = []
+    for i in range(num, num * sides + 1):
+        sums.append(0)
 
-# print sums
-max_sums = max_from_list(sums)
-s = ""
-for i in range(num, num * sides + 1):
-    s = s + " | " + str(i) + (" " * max_sums)
-print("\nSums  " + s)
-s = ""
-for i in range(num, num * sides + 1):
-    s = s + " | " + str(sums[i - num]) + (" " * (max_sums - get_length(sums[i - num]) + get_length(i)))
-print("      " + s)
+    # run trials
+    for i in range(0, trials):
+        for j in range(0, num):
+            rolls[roll_die(sides) - 1] += 1
+        s = 0
+        for j in range(num):
+            s += roll_die(sides)
+        sums[s - 1 - num] += 1
+
+    print("\n\n")
+    
+    # print rolls
+    max_roll_space = max_space(rolls)
+    highest_freq = max_list(rolls)
+    # graph
+    for i in range(highest_freq, 0, -1):
+        s = "      "
+        for j in rolls:
+            if j >= i:
+                s = s + (" " * 3) + "#" + (" " * max_roll_space)
+            else:
+                s = s + (" " * (max_roll_space + 4))
+        print(s)
+
+    # table
+    s = ""
+    for i in range(0, sides):
+        s = s + " | " + str(i + 1) + (" " * max_roll_space)
+    print("Rolls " + s)
+    s = ""
+    for i in range(0, sides):
+        s = s + " | " + str(rolls[i]) + (" " * (max_roll_space - get_length(rolls[i]) + get_length(i)))
+    print("      " + s)
+
+    # print sums
+    max_sum_space = max_space(sums)
+    highest_freq = max_list(sums)
+    
+    # table
+    s = ""
+    for i in range(num, num * sides + 1):
+        s = s + " | " + str(i) + (" " * max_sum_space)
+    print("\nSums  " + s)
+    s = ""
+    for i in range(num, num * sides + 1):
+        s = s + " | " + str(sums[i - num]) + (" " * (max_sum_space - get_length(sums[i - num]) + get_length(i)))
+    print("      " + s)
+
+    
+    # graph
+    for i in range(0, highest_freq):
+        s = "      "
+        for j in sums:
+            if j >= i:
+                s = s + (" " * 3) + "#" + (" " * max_sum_space)
+            else:
+                s = s + (" " * (max_sum_space + 4))
+        print(s)
+
